b68ea81854c458f5a0e7a4c34c7ef10534507ecb
ishidad2 <d.m.y.s.happy.life@gmail.com>
2022-07-04 14:45:13 +0900

    生きてるノードのチェック

--- js/nem_config.js
+++ js/nem_config.js
@@ -7,12 +7,13 @@ const NG_NODES = [
 //http://chain.nem.ninja/api3/nodes
 const NODES = [
   "https://00b21efd.xem.stir-hosyu.com:7891",
-  "https://nem-sakura-16.next-web-technology.com:7891",
   "https://symbolic.from.nagoya:7891",
   "https://super-nem.love:7891",
   "https://nis1.dusan.gq:7891",
   "https://nem-main-1.next-web-technology.com:7891",
   "https://6.dusan.gq:7891",
   "https://2.dusan.gq:7891",
-  "https://nis1.dusan.gq:7891"
-].filter(node => !NG_NODES.includes(node));;
+  "https://nis1.p-runner.net:7891",
+  "https://1.dusan.gq:7891",
+  "https://nis1.harvestasya.com:7891",
+].filter(node => !NG_NODES.includes(node));
