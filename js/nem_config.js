a75f547c76e7b8da223935e83778617a275fc017
ishidad2 <ishida_d@opening-line.co.jp>
2023-11-04 16:21:18 +0900

    接続先ノードリスト更新

--- js/nem_config.js
+++ js/nem_config.js
@@ -1,16 +1,24 @@
 const EXPLORER = "https://explorer.nemtool.com";
 
 const NG_NODES = [
-  "https://nis1.harvestasya.com:7891"
 ];
 
 //http://chain.nem.ninja/api3/nodes
 const NODES = [
-  "https://nem03.symbol-node.com:7891",
-  "https://nem01a.symbol-node.com:7891",
-  "https://nem06.symbol-node.com:7891",
-  "https://nem02.symbol-node.com:7891",
-  "https://nem04.symbol-node.com:7891",
-  "https://symbology.from.nagoya:7891",
-  "https://super-nem.love:7891"
+  'https://nem04.symbol-node.com:7891',
+  'https://4n.dusanjp.com:7891',
+  'https://nis1.dusanjp.com:7891',
+  'https://nem01a.symbol-node.com:7891',
+  'https://nem03.symbol-node.com:7891',
+  'https://2n.dusanjp.com:7891',
+  'https://6n.dusanjp.com:7891',
+  'https://7n.dusanjp.com:7891',
+  'https://nem06.symbol-node.com:7891',
+  'https://1n.dusanjp.com:7891',
+  'https://orange-pi5.symbol-node.com:7891',
+  'https://super-nem.love:7891',
+  'https://3n.dusanjp.com:7891',
+  'https://11n.dusanjp.com:7891',
+  'https://symbol.from.nagoya:7891',
+  'https://nem02.symbol-node.com:7891'
 ].filter(node => !NG_NODES.includes(node));
