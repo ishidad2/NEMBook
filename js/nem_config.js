827ef6244f66c969d1b2262b40231ccdc11fb149
ishidad2 <ishida_d@opening-line.co.jp>
2024-09-19 21:23:21 +0900

    ノードリスト修正

--- js/nem_config.js
+++ js/nem_config.js
@@ -5,20 +5,8 @@ const NG_NODES = [
 
 //http://chain.nem.ninja/api3/nodes
 const NODES = [
-  'https://nem04.symbol-node.com:7891',
-  'https://4n.dusanjp.com:7891',
-  'https://nis1.dusanjp.com:7891',
-  'https://nem01a.symbol-node.com:7891',
-  'https://nem03.symbol-node.com:7891',
-  'https://2n.dusanjp.com:7891',
-  'https://6n.dusanjp.com:7891',
-  'https://7n.dusanjp.com:7891',
-  'https://nem06.symbol-node.com:7891',
-  'https://1n.dusanjp.com:7891',
-  'https://orange-pi5.symbol-node.com:7891',
   'https://super-nem.love:7891',
-  'https://3n.dusanjp.com:7891',
-  'https://11n.dusanjp.com:7891',
-  'https://symbol.from.nagoya:7891',
-  'https://nem02.symbol-node.com:7891'
+  'https://luna2.dusanjp.com:7891',
+  'https://wasabisio.tsvr.net:7891',
+  'https://sika.dusanjp.com:7891'
 ].filter(node => !NG_NODES.includes(node));
