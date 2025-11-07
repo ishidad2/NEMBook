cdde0ea8e49a5c38a3ae8a1ec51a78f43380839c
ishidad2 <ishida_d@opening-line.co.jp>
2025-11-07 19:00:31 +0900

    サーバーリスト更新

--- js/nem_config.js
+++ js/nem_config.js
@@ -8,6 +8,6 @@ const NG_NODES = [
 const NODES = [
   'https://super-nem.love:7891',
   'https://luna2.dusanjp.com:7891',
-  'https://wasabisio.tsvr.net:7891',
+  // 'https://wasabisio.tsvr.net:7891',
   'https://sika.dusanjp.com:7891'
 ].filter(node => !NG_NODES.includes(node));
